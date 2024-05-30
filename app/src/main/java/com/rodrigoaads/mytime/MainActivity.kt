a41d102da30283946b7d9ab76c7b3485c4af5fcc
Rodrigo <rodrigo.dev17@gmail.com>
2024-05-30 09:34:26 -0300

    set app style

--- app/src/main/java/com/rodrigoaads/mytime/MainActivity.kt
+++ app/src/main/java/com/rodrigoaads/mytime/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.rodrigoaads.mytime.ui.theme.MyTimeTheme
 
@@ -34,7 +36,10 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        style = MaterialTheme.typography.titleLarge.copy(
+            fontWeight = FontWeight.Bold
+        )
     )
 }
 
